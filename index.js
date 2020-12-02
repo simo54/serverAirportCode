f182b74cce9d27b11ee554e227011e49a1f56531
simone <sorzesimone1@gmail.com>
2020-12-03 00:40:08 +0100

    fixing code=H20 Heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const file = require("./data.json");
 
@@ -30,6 +30,6 @@ app.get("/:code", (req, res) => {
 
 
 // Server listening
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log("Server running on port " + port);
 });
