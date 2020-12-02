d705f6873a65995c551028782a28b06826881597
simone <sorzesimone1@gmail.com>
2020-12-02 22:57:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+const bodyParser = require("body-parser");
+const file = require("./data.json");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+// Getting airport code and return json of the filtered result
+app.get("/:code", (req, res) => {
+  const { code } = req.params;
+  const check = file.filter((data) => {
+    return data.iata_code == code;
+  }); 
+  res.json(check);
+});
+
+// Server listening
+app.listen(8000, () => {
+  console.log("Server running on port " + port);
+});
